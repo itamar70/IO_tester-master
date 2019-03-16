95e74497436aec1df7f9c6798b003d1ffda78dff
itamar <itamarfalcon@gmail.com>
2019-03-16 18:42:03 +0200

    Upload files

---
+++ app/src/main/cpp/io-tester.h
@@ -0,0 +1,8 @@
+//
+// Created by lancerx on 4/20/18.
+//
+
+#ifndef IO_TESTER_IO_TESTER_H
+#define IO_TESTER_IO_TESTER_H
+
+#endif //IO_TESTER_IO_TESTER_H
---
+++ app/src/main/java/com/oscar/zhtao/io_tester/BootCompleteReceiver.java
@@ -0,0 +1,16 @@
+package com.oscar.zhtao.io_tester;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by tao on 6/29/16.
+ */
+public class BootCompleteReceiver extends BroadcastReceiver {
+    static public MainActivity main;
+
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
